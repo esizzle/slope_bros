8b4d824e046d3aab2e89f796cfca609f748e4d3f
esa <johnsonesa@gmail.com>
2026-03-10 14:11:16 -0600

    Added split screen and 2 player functionality

---
+++ Assets/Scripts/VehnicleControls2.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class VehicleControls2 : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rb;
+    [SerializeField] private float _force = 1f;
+
+    [SerializeField] private float _torque = 1f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private void FixedUpdate()
+    {
+        // if the player presses 's' increase the downward velocity of the rigidbody
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            _rb.AddForce(Vector2.down * _force);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _rb.AddTorque(1f * _torque);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _rb.AddTorque(-1f * _torque);
+        }
+        
+    }
+}
